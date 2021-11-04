5be275e9d90e634ab819acca38960ba9426425cb
vskarasev <karasev997@mail.ru>
2021-11-04 18:07:32 +0500

    Задания урока #5

---
+++ lesson_5/logs/server_logs_config.py
@@ -0,0 +1,34 @@
+"""Кофнфиг серверного логгера"""
+
+import sys
+import os
+import logging
+import lesson_5.common.variables as variables
+
+# Подготовка имени файла для логирования
+LOG_DIRECTORY = os.path.join(os.path.dirname(os.path.abspath(__file__)), variables.LOG_DIRECTORY)
+LOG_FILE = os.path.join(LOG_DIRECTORY, variables.LOG_SERVER_NAME)
+
+# создаём регистратор и настраиваем его
+LOGGER = logging.getLogger('server')
+LOGGER.setLevel(logging.DEBUG)
+
+FORMAT = logging.Formatter('%(asctime)-10s %(levelname)-10s %(filename)-10s %(message)s')
+
+STDERR_HANDLER = logging.StreamHandler(sys.stderr)
+STDERR_HANDLER.setLevel(logging.INFO)
+STDERR_HANDLER.setFormatter(FORMAT)
+
+FILE_HANDLER = logging.FileHandler(LOG_FILE)
+FILE_HANDLER.setLevel(logging.DEBUG)
+FILE_HANDLER.setFormatter(FORMAT)
+
+LOGGER.addHandler(STDERR_HANDLER)
+LOGGER.addHandler(FILE_HANDLER)
+
+if __name__ == '__main__':
+    LOGGER.critical('Критическая ошибка')
+    LOGGER.error('Ошибка')
+    LOGGER.debug('Отладочная информация')
+    LOGGER.info('Информационное сообщение')
+    LOGGER.warning('Внимание')
